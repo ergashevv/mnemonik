b32582780dd9919715b43f77e6b96ddba0905c43
ergashevv <ergashevpulot@gmail.com>
2022-10-12 13:25:11 +0500

    adsa

--- src/components/numbers-components/start-game/index.tsx
+++ src/components/numbers-components/start-game/index.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react"
+import { useHomeContext } from "../../../context/home-context"
 import "./main.scss"
 export interface TimeProps {
   time: number
 }
 const StartGameModal = ({ time }: TimeProps) => {
+  const { startTime, setStartTime } = useHomeContext()
+  useEffect(() => {
+    const timeout: NodeJS.Timeout = setTimeout(() => {
+      const newTime = startTime > 0 ? startTime - 1 : startTime
+      setStartTime(newTime)
+      if (newTime === 0) clearTimeout(timeout)
+    }, 1000)
+    return () => clearTimeout(timeout)
+  }, [setStartTime, startTime])
   return (
     <>
       <div className="start-game">
